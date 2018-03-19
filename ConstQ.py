85057216a2e04872b04d5d5c3971415e3cc84b4c
AlexsaniKy <AlexsaniWilliams@gmail.com>
2018-03-19 15:23:19 -0400

    created signal function

--- ConstQ.py
+++ ConstQ.py
@@ -1,10 +1,19 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-x = np.linspace(-np.pi, np.pi, 201)
+def create_signal(t, freq, amp, phase):
+	return np.sin(t * freq * 2*np.pi + phase)*amp
+
+t_0 = 0
+t_f = 10
+steps = 200
+
+t = np.linspace(t_0, t_f, steps)
 
 freq = 1
 amp = 1
+phase = 0
 
-plt.plot(x, np.sin(x*freq)*amp)
+plt.plot(t, create_signal(t, 1, 1, 0))
+plt.plot(t, create_signal(t, 1, 1, np.pi/2))
 plt.show()
